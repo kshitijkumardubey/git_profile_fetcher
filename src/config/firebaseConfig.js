c70618050b07ae8a3d5ced07e25b40b0c23835b3
Kshitij <dkshitijkumar98@gmail.com>
2020-11-28 12:05:24 +0530

    git profile info

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default  {
+    apiKey: "AIzaSyCODBLy1bSeOadg9YSVc8r3wVkoXDGR7vg",
+    authDomain: "mygitapp-85b37.firebaseapp.com",
+    databaseURL: "https://mygitapp-85b37.firebaseio.com",
+    projectId: "mygitapp-85b37",
+    storageBucket: "mygitapp-85b37.appspot.com",
+    messagingSenderId: "471964131717",
+    appId: "1:471964131717:web:1ae4631bd333121f2ba4ec",
+    measurementId: "G-K80S7QZ1HP"
+  };
